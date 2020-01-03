ecae37126175a1b6c5745e5b75dc33560e4c43f4
万古云霄 <zhangpanqin@outlook.com>
2020-01-04 00:02:02 +0800

    build:解决 hoek 依赖警告问题，升级了 less, less-loader

--- src/store/index.js
+++ src/store/index.js
@@ -5,23 +5,11 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        count: 0,
     },
     mutations: {
-        Add_Count(state, playload) {
-            state.count += playload;
-        },
     },
     actions: {
-        Add_Count({ commit }, playload) {
-            commit('Add_Count', playload);
-        },
     },
     modules: {
-    },
-    getters: {
-        countAdd1(state) {
-            return state.count + 1;
-        },
     },
 });
